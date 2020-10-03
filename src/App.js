baf85e6405f5a33800647b6ac12fdc96b7431178
frontendprof <abdumaliksharipov@gmail.com>
2020-10-03 21:01:48 +0500

    Initial setup of todoapp list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h2>Todo App</h2>
+      
+    </div>
+  );
+}
+
+export default App;
