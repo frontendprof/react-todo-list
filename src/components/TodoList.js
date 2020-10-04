e0a0cadc91960b28f74034503396cedb32c417ab
frontendprof <abdumaliksharipov@gmail.com>
2020-10-04 20:49:43 +0500

    Working on Todo component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,7 @@
 import React,{useState} from 'react';
 
 import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoList() {
 
@@ -18,12 +19,24 @@ function TodoList() {
         console.log(todo,newTodos);
     }
 
+    const completeTodo=id=>{
+        
+        let updatedTodos=todos.map(todo=>{
+            if(todo.id===id){
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo
+        });
+        setTodos(updatedTodos);
+    }
+
 
 
     return (
         <div>
             <h1>What is the plan for today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo}/>
             
         </div>
     )
